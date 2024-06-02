fc07c6aeb1b48acc59f3f170549c43a0380d5f02
zhuzihan728 <zcabhub@ucl.ac.uk>
2024-06-03 00:41:23 +0100

    add adp_v2. update score analysis

--- selfrag-selfeval.py
+++ selfrag-selfeval.py
@@ -35,7 +35,6 @@ torch.cuda.manual_seed_all(seed)
 
 def preprocess_input_data(dataset, res, task=None):
     new_data = []
-    assert len(dataset) == len(res)
     for ind, item in enumerate(dataset):
         if task == "arc_c":
             choices = item["choices"]
@@ -65,35 +64,33 @@ def preprocess_input_data(dataset, res, task=None):
             item["instruction"] = f"Is the claim \"{item['question']}\" true or false?"
         else:
             item["instruction"] = item["question"]
-        new_data.append({'instruction': f'Query: {item["instruction"]}\nAnswer: {res[ind]}'})
+        assert len(res[ind]) == 5
+        queries = '\n'.join([f"#{i+1}: {v}" for i, v in enumerate(res[ind])])
+        new_data.append({'instruction': f'Query: {item["instruction"]}\nCandidate answers:\n{queries}'})
 
     return new_data
 
-def get_retrieval_p(pred, tokenizer):
-    global YES_TOKEN, NO_TOKEN
-    has_judgment = False
-    judge_ind = 0
-    retrieve_p_hard = None
+def get_scores(pred, tokenizer):
+    global TOKEN_1, TOKEN_2, TOKEN_3, TOKEN_4, TOKEN_5
+    has_selection = False
+    selection_ind = 0
+    select_hard = None
     for ind, id_ in enumerate(pred.outputs[0].token_ids):
-        print(f"{id_}|{tokenizer.decode(id_)}")
-        print(YES_TOKEN, NO_TOKEN)
-        print(tokenizer.encode('Yes'), tokenizer.encode('No'))
+        raw_word = tokenizer.decode(id_)
+        print(f"target id: {id_}|{raw_word}|")
+        print(TOKEN_1, TOKEN_2, TOKEN_3, TOKEN_4, TOKEN_5)
         word = tokenizer.decode(id_).strip().lower()
-        if id_ in [YES_TOKEN, NO_TOKEN]:
-            has_judgment = True
-            judge_ind = ind
-            if word == 'yes':
-                retrieve_p_hard = 1
-            else:
-                retrieve_p_hard = 0
+        if id_ in [TOKEN_1, TOKEN_2, TOKEN_3, TOKEN_4, TOKEN_5]:
+            print(f"|{raw_word}|")
+            has_selection = True
+            selection_ind = ind
+            select_hard = word
             break
-    log_prob_dc = pred.outputs[0].logprobs[judge_ind] # use the first token if no judgment
-    retrieve_p = (np.exp(log_prob_dc[YES_TOKEN])/(np.exp(log_prob_dc[YES_TOKEN])+np.exp(log_prob_dc[NO_TOKEN])))
-    if retrieve_p_hard is None:
-        retrieve_p_hard = float(retrieve_p>0.5)
-        
-    probs = {'yes': np.exp(log_prob_dc[YES_TOKEN]), 'no': np.exp(log_prob_dc[NO_TOKEN])}
-    return retrieve_p, retrieve_p_hard, has_judgment, probs
+    log_prob_dc = pred.outputs[0].logprobs[selection_ind] # use the first token if no judgment
+    select_soft = {'1': np.exp(log_prob_dc[TOKEN_1]), '2': np.exp(log_prob_dc[TOKEN_2]), '3': np.exp(log_prob_dc[TOKEN_3]), '4': np.exp(log_prob_dc[TOKEN_4]), '5': np.exp(log_prob_dc[TOKEN_5])}
+    if select_hard is None:
+         select_hard = 'FAILED'
+    return select_hard, select_soft, has_selection
 
 @singledispatch
 def to_serializable(val):
@@ -129,26 +126,23 @@ def main():
     with open(res_path, 'r') as f:
         res = json.load(f)
         res = res['results']
-        res = [i['no_retrieval'] for i in res]
+
+    temp_res = []
+    for i in res:
+        temp = [v['pred'] for v in i['retrieval_res'].values()]
+        temp_res.append(temp)
+    res = temp_res
     
     model_ = AutoModelForCausalLM.from_pretrained(gpt, device_map='auto', torch_dtype=torch.bfloat16)
     tokenizer = AutoTokenizer.from_pretrained(gpt, padding_side="left")
     max_new_tokens = args.max_new_tokens
     model = MyModel(model_, tokenizer, max_new_tokens=max_new_tokens)
-    global YES_TOKEN, NO_TOKEN
+    global TOKEN_1, TOKEN_2, TOKEN_3, TOKEN_4, TOKEN_5
+    print('tokenized number 1-5:', tokenizer.encode("1"), tokenizer.encode("2"), tokenizer.encode("3"), tokenizer.encode("4"), tokenizer.encode("5"))
     if "Llama-2" in args.model_name:
-        YES_TOKEN = 8241
-        NO_TOKEN = 3782
+        TOKEN_1, TOKEN_2, TOKEN_3, TOKEN_4, TOKEN_5 = tokenizer.encode("1")[2], tokenizer.encode("2")[2], tokenizer.encode("3")[2], tokenizer.encode("4")[2], tokenizer.encode("5")[2]
     else:
-        YES_TOKEN = tokenizer.encode('Yes')[1]
-        NO_TOKEN = tokenizer.encode('No')[1]
-    
-    if "Llama-3" in args.model_name:
-        prefix_len = len(tokenizer.decode(128006) + tokenizer.decode(78191) + tokenizer.decode(128007) + tokenizer.decode(271))
-        res = [i[prefix_len:] for i in res]
-        
-    res = [i.strip() for i in res]
-    
+        TOKEN_1, TOKEN_2, TOKEN_3, TOKEN_4, TOKEN_5 = tokenizer.encode("1")[1], tokenizer.encode("2")[1], tokenizer.encode("3")[1], tokenizer.encode("4")[1], tokenizer.encode("5")[1]
     input_data = preprocess_input_data(input_data, res, task=args.task)
     
     
@@ -156,34 +150,36 @@ def main():
     dataloader = DataLoader(dataset, batch_size=4, shuffle=False)
     
     # instruction = "Given the above user query, please make a judgment on whether you need some external documents from the web (e.g., Wikipedia) to correct answer it. Please answer yes or no."
-    instruction = "Based on the given query and answer, do you think consulting external sources like Wikipedia would provide a more accurate answer? Please respond ONLY with #Yes or #No."
+    instruction = "Given a query and five candidate answers, please select the most accurate answer to the query. Reply one of the following: #1, #2, #3, #4, #5. Then, explain your choice."
     
-    res = {"retrieval_p":[], 'retrieval_p_hard':[], 'has_judgment':[], 'probs':[]}
+    res = {'prompts': [], 'answers': [], 'scores': []}
     for ind, batch in enumerate(dataloader):
-        prompts = [f"{i}\n\n{instruction}" for i in batch['instruction']]
+        prompts = [f"{instruction}\n\n{i}" for i in batch['instruction']]
         chats = [[{"role": "user", "content": i}] for i in prompts]
         if "Llama-3" in args.model_name:
-            response_prefix = tokenizer.decode(78191) + tokenizer.decode(271)
+            response_prefix = tokenizer.decode(128006) + tokenizer.decode(78191) + tokenizer.decode(128007) + tokenizer.decode(271)
             prompts = [tokenizer.apply_chat_template(chat, tokenize=False)+response_prefix for chat in chats]
-            prompts = [i + '#' for i in prompts]
+            prompts = [tokenizer.apply_chat_template(chat, tokenize=False) + 'The most accurate answer is #' for chat in chats]
         elif "Llama-2" in args.model_name:
             prompts = [tokenizer.apply_chat_template(chat, tokenize=False) for chat in chats]
-            prompts = [i + '#' for i in prompts]
+            prompts = [tokenizer.apply_chat_template(chat, tokenize=False) + 'The most accurate answer is #' for chat in chats]
+        elif 'selfrag' in args.model_name:
+            template = "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n"
+            prompts = [template.format(instruction=instruction, input=i)+'The most accurate answer is #' for i in batch['instruction']]
         else:
             raise NotImplementedError
-        
         pred = model.generate(prompts)
         
         for i, p in enumerate(pred):
-            retrieve_p, retrieve_p_hard, has_judgment, probs = get_retrieval_p(p, tokenizer)
-            res['retrieval_p'].append(retrieve_p)
-            res['retrieval_p_hard'].append(retrieve_p_hard)
-            res['has_judgment'].append(has_judgment)
-            res['probs'].append(probs)
+            select_hard, select_soft, has_selection = get_scores(p, tokenizer)
+            res['prompts'].append(prompts[i])
+            res['answers'].append(select_hard)
+            res['scores'].append(select_soft)
+            
             print(f'===================== Batch {ind}, item {i} =====================')
-            print(f"query: {prompts[i]}")
-            print(f"judgment: {p.outputs[0].text}")
-            print(f"retrieval_p: {retrieve_p}, retrieval_p_hard: {retrieve_p_hard}, has_judgment: {has_judgment}, probs: {probs}")
+            print(f"prompt: {prompts[i]}")
+            print(f"selected: {select_hard}")
+            print(f"scores: {select_soft}")
     
         if ind%100== 0:
             with open(args.output_file, 'w') as f:
